915701ba563c6cfa86eac1970debd594ad654db8
changyoung <changyoung@toss.im>
2022-11-02 15:55:23 +0900

    init workspace

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  env: { es6: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'import', 'simple-import-sort', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  rules: {
+    //
+    // eslint-plugin-import
+    //
+
+    // disallow non-import statements appearing before import statements
+    'import/first': 'error',
+    // Require a newline after the last import/require in a group
+    'import/newline-after-import': 'error',
+    // Forbid import of modules using absolute paths
+    'import/no-absolute-path': 'error',
+    // disallow AMD require/define
+    'import/no-amd': 'error',
+    // forbid default exports - we want to standardize on named exports so that imported names are consistent
+    'import/no-default-export': 'error',
+    // disallow imports from duplicate paths
+    'import/no-duplicates': 'error',
+    // Forbid the use of extraneous packages
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        peerDependencies: true,
+        optionalDependencies: false,
+      },
+    ],
+    // Forbid mutable exports
+    'import/no-mutable-exports': 'error',
+    // Prevent importing the default as if it were named
+    'import/no-named-default': 'error',
+    // Prohibit named exports
+    'import/no-named-export': 'off', // we want everything to be a named export
+    // Forbid a module from importing itself
+    'import/no-self-import': 'error',
+    // Require modules with a single export to use a default export
+    'import/prefer-default-export': 'off', // we want everything to be named
+
+    // enforce a sort order across the codebase
+    'simple-import-sort/imports': 'error',
+  },
+};
