84d773a9f3e8f1fa6782cf6adaa07050cb5c6bb8
changyoung <changyoung@toss.im>
2022-11-08 14:37:16 +0900

    fix: Parse config by zod schema

--- packages/core/src/contexts/Config.ts
+++ packages/core/src/contexts/Config.ts
@@ -4,7 +4,7 @@ import { PackageJSONSchema } from '../models/PackageJSON';
 
 export const DEFAULT_ORDER = PackageJSONSchema.keyof().options;
 
-const ConfigSchema = z.object({
+export const ConfigSchema = z.object({
   order: z
     .array(z.union([PackageJSONSchema.keyof(), z.literal('...')]))
     .default(PackageJSONSchema.keyof().options)
@@ -13,6 +13,7 @@ const ConfigSchema = z.object({
     .array(PackageJSONSchema.keyof())
     .default(['dependencies', 'devDependencies', 'peerDependencies', 'resolutions'])
     .optional(),
+  required: z.array(PackageJSONSchema.keyof()).default(['name']).optional(),
 });
 
 export type ConfigType = z.infer<typeof ConfigSchema>;
