018dd7cabf5582f0879f689deebe4caab980e948
changyoung <changyoung@toss.im>
2022-11-09 23:41:29 +0900

    fix: add default option to ConfigSchema

--- packages/core/src/contexts/Config.ts
+++ packages/core/src/contexts/Config.ts
@@ -4,19 +4,21 @@ import { PackageJSONSchema } from '../models/PackageJSON';
 
 export const DEFAULT_ORDER = PackageJSONSchema.keyof().options;
 
-export const ConfigSchema = z.object({
-  order: z
-    .array(z.union([PackageJSONSchema.keyof(), z.literal('...')]))
-    .default(PackageJSONSchema.keyof().options)
-    .optional(),
-  deep: z
-    .array(PackageJSONSchema.keyof())
-    .default(['dependencies', 'devDependencies', 'peerDependencies', 'resolutions'])
-    .optional(),
-  required: z.array(PackageJSONSchema.keyof()).default(['name']).optional(),
-  include: z.array(z.string()).default(['./packages/**']).optional(),
-  exclude: z.array(z.string()).default(['./packages/**']).optional(),
-});
+export const ConfigSchema = z
+  .object({
+    order: z
+      .array(z.union([PackageJSONSchema.keyof(), z.literal('...')]))
+      .default(PackageJSONSchema.keyof().options)
+      .optional(),
+    deep: z
+      .array(PackageJSONSchema.keyof())
+      .default(['dependencies', 'devDependencies', 'peerDependencies', 'resolutions'])
+      .optional(),
+    required: z.array(PackageJSONSchema.keyof()).default(['name']).optional(),
+    include: z.array(z.string()).default(['./packages/**']).optional(),
+    exclude: z.array(z.string()).default(['./packages/**']).optional(),
+  })
+  .default({});
 
 export type ConfigType = z.infer<typeof ConfigSchema>;
 export const DefaultConfig = ConfigSchema.parse({});
