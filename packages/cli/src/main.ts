d19466137c2a3dcdcefddfda60125841df4d3750
ho991217 <ho991217@kakao.com>
2025-12-26 17:27:52 +0900

    feat: Overhaul command
    Removed @packlint/command package and replace clipanion with commander.

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,37 +1,10 @@
-import { AllCommand, MergeCommand, SortCommand } from '@packlint/command';
-import { ConfigType, getConfig } from '@packlint/core';
-import { BaseContext, Cli } from 'clipanion';
+import { Command } from 'commander';
+import packageJson from '../package.json' with { type: 'json' };
 
 export async function main() {
-  async function run() {
-    const cli = new Cli<BaseContext & { config: ConfigType }>({
-      binaryName: 'packlint',
-      binaryLabel: 'Package.json Manager',
-      binaryVersion: '0.0.1',
-    });
+  const program = new Command();
 
-    try {
-      await exec(cli);
-    } catch (error) {
-      process.stdout.write(cli.error(error));
-      process.exitCode = 1;
-    }
-  }
+  program.name('packlint').description('Package.json Linter for monorepos').version(packageJson.version);
 
-  async function exec(cli: Cli<BaseContext & { config: ConfigType }>) {
-    const config = await getConfig();
-
-    cli.register(SortCommand);
-    cli.register(MergeCommand);
-    cli.register(AllCommand);
-
-    const command = cli.process(process.argv.slice(2), { config });
-
-    return cli.runExit(command, { config });
-  }
-
-  return run().catch(error => {
-    process.stdout.write(error.message);
-    process.exitCode = 1;
-  });
+  program.parse(process.argv);
 }
